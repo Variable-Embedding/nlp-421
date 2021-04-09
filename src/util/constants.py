c84273bc8496f790b44ccb92f41e305e8cc970c3
justinhchae <justin.chae@nlaw.northwestern.edu>
2021-04-09 12:37:32 -0400

    Update constants.py

--- src/util/constants.py
+++ src/util/constants.py
@@ -4,9 +4,9 @@ import os
 from os.path import dirname
 
 WORKFLOW_ROOT = dirname(dirname(dirname(__file__)))
-DATA_FOLDER = os.sep.join([os.environ['PWD'], 'data'])
+DATA_FOLDER = os.sep.join([WORKFLOW_ROOT, 'data'])
 EMBEDDING_FOLDER = os.sep.join([DATA_FOLDER, 'embeddings'])
-LOGGING_PATH = os.sep.join([os.environ['PWD'], 'logs'])
+LOGGING_PATH = os.sep.join([WORKFLOW_ROOT, 'logs'])
 
 GLOVE_TWITTER = "http://nlp.stanford.edu/data/glove.twitter.27B.zip"
 COMMMON_CRAWL_840B = "http://nlp.stanford.edu/data/glove.840B.300d.zip"
