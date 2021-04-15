2adad499971a2e64fa201daeeb05b0102eed27e6
justinhchae <justin.chae@nlaw.northwestern.edu>
2021-04-14 20:30:28 -0400

    push initial torchtext based workflow

--- src/runners/run_dataprep.py
+++ src/runners/run_dataprep.py
@@ -1,45 +1,29 @@
 from src.stages.stage_prep_corpus import stage_prep_corpus
 from src.stages.stage_prep_embedding import stage_prep_embedding
+from src.util.get_embeddings import prep_corpus_embeddings
 from src.util.constants import *
 import logging
 
 
-def run_dataprep(embedding_type, corpus_type='gutenberg', target_glove=None):
+def run_dataprep(embedding_type, corpus_type, target_glove=None):
     """
-    A runner to prepare data for nlp pipeline, consolidate stages and helper functions
-
-    :param embedding_type: string, a key name for a type of GloVe embedding to download and prep
-        embedding_type is one of: "glove_twitter", "glove_common_crawl", or "840B" which is the same as glove_common_crawl
-        If "glove_common_crawl" or "840B" are selected, then the target glove is defaulted to "glove.840B.300d.pickle"
-        and embedding size is default to 300 (based on glove params).
-    :param target_glove: the target pre-trained word embeddings to use
-    :param corpus_type: string, default to "gutenberg" for a small, manageable toy dataset.
-                    Other Options: "wiki_text_2"
-    :return nn_embedding data and the target corpra:
-
-    all data required to start nlp-pipeline as a dictionary
-
-    :nn_embedding:    {"emb_layer": emb_layer
-                      , "num_embeddings": num_embeddings
-                      , "embedding_dim": embedding_dim
-                      , "word2idx": word2idx
-                      , "idx2word": idx2word
-                      , "vectors": vectors
-                        }
-
-    :target_corpra: a list of tokens representing the toy dataset of books
     """
-    # should be some function that returns the target vocabulary
-    target_vocab, target_corpra = stage_prep_corpus(corpus_type=corpus_type)
+    # pre-trained embedding -> {"word2idx": word2idx, "idx2word": idx2word, "vectors": vectors}
+    embeddings = stage_prep_embedding(embedding_type=embedding_type, target_glove=target_glove)
+    vocabulary = stage_prep_corpus(corpus_type)
 
-    # prepares dictionaries of {word : embedding vectors}
-    embeddings = stage_prep_embedding(embedding_type=embedding_type
-                                      , target_glove=target_glove
-                                      , target_vocab=target_vocab)
+    corpus_embeddings = {"word2idx": vocabulary['train'].stoi
+                         , "vectors": embeddings['vectors']
+                         , "target_vocab": vocabulary['train'].itos}
 
-    logging.info(f'Stage Data Prep Complete, returning embedding dictionaries and corpra\n')
+    nn_data = prep_corpus_embeddings(**corpus_embeddings)
+    nn_data.update({'vocabulary': vocabulary})
 
-    return embeddings, target_corpra
+    logging.info(f'Stage Data Prep Complete, returning nn_data:\n')
+    for k, v in nn_data.items():
+        logging.info(f'\tDictionary key: {k}, type: {type(v)}, with len: {len(v)}')
+
+    return nn_data
 
 
 
